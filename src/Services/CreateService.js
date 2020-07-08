74930b51f4c467561ba37f1df85e0748f63cd18c
AkangAkang <975376945@qq.com>
2020-07-08 09:20:20 +0800

    repair

---
+++ src/Services/CreateService.js
@@ -0,0 +1,6 @@
+import {postRequest,getRequest,deleteRequest} from "./ajax";
+
+export const getBatchOneToOne=(data,callback)=>{
+    const url = `http://localhost:8080/getShort?id=1`;
+    postRequest(url, data,callback);
+};
