4e0d5434d3b5a9b8493cdbc9fbfe72c7d131b0be
zhanghanchong <308011618@qq.com>
2020-07-09 16:30:16 +0800

    Version 1.0 (fixed bugs to communicate between backend and frontend)

--- src/Services/CreateService.js
+++ src/Services/CreateService.js
@@ -3,6 +3,16 @@ import {postRequest,getRequest,deleteRequest} from "./ajax";
 
 
 export const getBatchOneToOne=(data,callback)=>{
-    const url = `http://localhost:8080/getShort?id=1`;
+    const url = `http://localhost:4000/getShort?id=1`;
     postRequest(url, data,callback);
+    // getRequest(url,callback);
+};
+
+export const getBatchManyToOne=(data,callback)=>{
+    const url = `http://localhost:4000/getOneShort?id=1`;
+    postRequest(url, data,callback);
+    // getRequest(url,callback,{
+    //     params:{id:0},
+    //     errorCallback:{}
+    // });
 };
