24300b08351e6e2ad59a8cb5a7f1076d7ef95dfe
aoao <ao7777@sjtu.edu.cn>
2020-07-14 16:53:10 +0800

    fix: solved a problem that causes ManyToOneTable produce duplicates of QR code feat: QR code transformation is now available for OneToOneTable.js

---
+++ src/components/create/ShortWithQR.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { hostUrl } from "../../Services/ajax";
+import { Popover, Spin } from "antd";
+import { QrcodeOutlined } from "@ant-design/icons";
+/*ShortWithQR:
+@author Zhuohao Shen <ao7777@sjtu.edu.cn>
+@date July 14th 2020
+@description a little icon transfer links into QR code, using API from qrserver
+@params {String} short - short link identifier code
+@contents {DOM} - an icon with popover showing a QR code
+*/
+export default class ShortWithQR extends Component {
+    state = {
+        loaded: false,
+    };
+    handleQRload = () => {
+        this.setState({ loaded: true });
+    };
+
+    AsyncQRcode = (value) => {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <img
+                    src={
+                        "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+                        hostUrl +
+                        "/" +
+                        value
+                    }
+                    onLoad={this.handleQRload}
+                    alt={""}
+                />
+                {this.state.loaded ? null : <Spin size="large" />}
+            </div>
+        );
+    };
+    render() {
+        return (
+            <div>
+                {this.props.value}
+                {this.props.value === "" ? null : (
+                    <Popover
+                        content={this.AsyncQRcode(this.props.value)}
+                        title="生成二维码"
+                    >
+                        <QrcodeOutlined />
+                    </Popover>
+                )}{" "}
+            </div>
+        );
+    }
+}
