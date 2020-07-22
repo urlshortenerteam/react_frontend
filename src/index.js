6c7195eaf54a5f9fa10621c1f5adddca3ea345ab
aoao <ao7777@sjtu.edu.cn>
2020-07-22 13:23:21 +0800

    fix: prettier

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "antd/dist/antd.css";
 import * as serviceWorker from "./serviceWorker";
 import BasicRouter from "./router/BasicRouter";
 
-
 ReactDOM.render(<BasicRouter />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
