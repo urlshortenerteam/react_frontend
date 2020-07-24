3cd42ba19b52b3961e63016a5eb0246557c47ea1
AkangAkang <975376945@qq.com>
2020-07-24 14:55:31 +0800

    fix : getReq for refresh feat : time change added feat : route history feat : show rank

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -230,7 +230,7 @@ let getRequest_checkSession = (url, callback, { errorCallback, params }) => {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-           " Authorization": JSON.parse(sessionStorage.getItem("user"))
+            Authorization: JSON.parse(sessionStorage.getItem("user"))
                 ? JSON.parse(sessionStorage.getItem("user")).token
                 : null,
             "Cache-Control": "no-cache"
