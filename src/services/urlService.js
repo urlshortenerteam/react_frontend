333c18b77ed8a795913d1d6321474e40457520b5
aoao <ao7777@sjtu.edu.cn>
2020-07-26 00:21:59 +0800

    fix: urlService.js rollback

--- src/services/urlService.js
+++ src/services/urlService.js
@@ -11,10 +11,17 @@ import { getRequest, postRequest } from "./ajax";
  * @param {function} parameters.errorCallback - The callback for errors
  * */
 let BanUrl = ({ url, callback, errorCallback }) => {
-    postRequest("/editUrl", "BANNED", callback, {
-        errorCallback: errorCallback,
-        params: { id: sessionStorage.getItem("userId"), shortUrl: url },
-    });
+    postRequest(
+        "/editUrl",
+        {
+            longUrl: "BANNED",
+            shortUrl: url,
+        },
+        callback,
+        {
+            errorCallback: errorCallback,
+        }
+    );
 };
 /**
  * EditUrl
@@ -30,13 +37,17 @@ let BanUrl = ({ url, callback, errorCallback }) => {
 let EditUrl = ({ url, newLong, callback, errorCallback }) => {
     console.log(newLong);
     console.log(url);
-    postRequest("/editUrl", newLong, callback, {
-        errorCallback: errorCallback,
-        params: {
-            id: JSON.parse(sessionStorage.getItem("userId")),
+    postRequest(
+        "/editUrl",
+        {
+            longUrl: newLong,
             shortUrl: url,
         },
-    });
+        callback,
+        {
+            errorCallback: errorCallback,
+        }
+    );
 };
 /**
  * LiftUrl
@@ -49,13 +60,18 @@ let EditUrl = ({ url, newLong, callback, errorCallback }) => {
  * @param {function} parameters.errorCallback - The callback for errors
  * */
 let LiftUrl = ({ url, callback, errorCallback }) => {
-    postRequest("/editUrl", "LIFT", callback, {
-        errorCallback: errorCallback,
-        params: {
-            id: JSON.parse(sessionStorage.getItem("userId")),
+    postRequest(
+        "/editUrl",
+        {
+            longUrl: "LIFT",
             shortUrl: url,
         },
-    });
+        callback,
+        {
+            errorCallback: errorCallback,
+            params: {},
+        }
+    );
 };
 /**
  * GetUrl
@@ -71,7 +87,9 @@ let GetUrl = ({ url, callback, errorCallback }) => {
     getRequest("/getShortStat", callback, {
         errorCallback: errorCallback,
         params: {
-            id: JSON.parse(sessionStorage.getItem("userId")),
+            id: sessionStorage.getItem("user")
+                ? JSON.parse(sessionStorage.getItem("user")).id
+                : null,
             short: url,
         },
     });
